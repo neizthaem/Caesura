52656cf85643494dfd7fc008506230878ed46c4d
Zachary Stewart <stewarzt@rose-hulman.edu>
2013-03-20 14:05:28 -0400

    Initial Test Created

---
+++ Caesura/Caesura/UserRegTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Caesura
+{ 
+    [TestFixture()]
+    class UserRegTests
+    {
+
+        [Test()]
+        public void initializeTest()
+        {
+            User newUser = new User();
+            Assert.IsNotNull(newUser);
+        }
+
+        [Test()]
+        public void nameAcceptTest()
+        {
+
+        }
+
+
+
+    }
+}
+
+        //[Test()]
+        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
