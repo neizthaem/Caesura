0bcc5ddea12fbaf3810dded51f8fb573e53dc218
Zachary Stewart <stewarzt@rose-hulman.edu>
2013-03-20 14:15:34 -0400

    Initializes the Class Properly

---
+++ Caesura/Caesura/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Caesura
+{
+    class User
+    {
+        private String name;
+        private String password;
+
+
+        public User()
+        {
+        }
+
+
+
+    }
+}
