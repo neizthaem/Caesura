534e05ce5b07eba7f301574057127bd9689ee9bd
neizthaem <uphusar@rose-hulman.edu>
2013-03-21 12:02:10 -0400

    first test case skeletons and fixed pathing

--- Caesura/Caesura/Search.cs
+++ Caesura/Caesura/Search.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,21 @@ using System.Threading.Tasks;
 
 namespace Caesura
 {
-    class Search
+    public class Search
     {
+
+        /**
+         * Returns true if the file exists false otherwise
+         */
+        public static Boolean FileExists(String path, String file)
+        {
+            return false;
+        }
 
         /**
          * Returns all of tags associated with the file at path
          **/
-        public List<String> GetTags(String path, String file)
+        public static List<String> GetTags(String path, String file)
         {
             return null;
         }
@@ -20,14 +29,14 @@ namespace Caesura
         /**
          * Returns a list of files at the path that contain each tag(s)
          **/
-        public List<String> GetFilesTaggedAs(String path, params String[] tags) {
+        public static List<String> GetFilesTaggedAs(String path, params String[] tags) {
             return null;
         }
 
         /**
          * Returns a list of files at the path that at least one of the tag(s)
          **/
-        public List<String> GetFilesContainingTags(String path, params String[] tags)
+        public static List<String> GetFilesContainingTags(String path, params String[] tags)
         {
             return null;
         }
---
+++ Caesura/CaesuraTest/SearchTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Caesura;
+using NUnit.Framework;
+
+namespace CaesuraTest
+{
+    [TestFixture()]
+    public class SearchTest
+    {
+
+        [Test()]
+        public void TestFileExists()
+        {
+            Boolean b = Search.FileExists("SearchTestFiles/", "test_file.txt");
+            Assert.AreEqual(true, Search.FileExists("SearchTestFiles/", "test_file.txt"));
+        }
+
+        [Test()]
+        public void TestGetTags()
+        {
+
+        }
+
+        [Test()]
+        public void TestFilesTaggedAs()
+        {
+
+        }
+
+        [Test()]
+        public void TestFilesContainingTags()
+        {
+
+        }
+
+    }
+}
