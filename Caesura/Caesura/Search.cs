36a01d616fdffdcf2fad7b9586d918748f93efa3
neizthaem <uphusar@rose-hulman.edu>
2013-03-21 11:25:12 -0400

    Skeleton search and test cases added

---
+++ Caesura/Caesura/Search.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesura
+{
+    class Search
+    {
+
+        /**
+         * Returns all of tags associated with the file at path
+         **/
+        public List<String> GetTags(String path, String file)
+        {
+            return null;
+        }
+
+        /**
+         * Returns a list of files at the path that contain each tag(s)
+         **/
+        public List<String> GetFilesTaggedAs(String path, params String[] tags) {
+            return null;
+        }
+
+        /**
+         * Returns a list of files at the path that at least one of the tag(s)
+         **/
+        public List<String> GetFilesContainingTags(String path, params String[] tags)
+        {
+            return null;
+        }
+
+
+
+    }
+}
---
+++ Caesura/ConsoleApplication1/SearchTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Caesura;
+using NUnit.Framework;
+
+namespace ConsoleApplication1
+{
+    [TestFixture()]
+    public class SearchTest
+    {
+        [Test()]
+        public void TestGetTags()
+        {
+            
+        }
+
+        [Test()]
+        public void TestFilesTaggedAs()
+        {
+
+        }
+
+        [Test()]
+        public void TestFilesContainingTags()
+        {
+
+        }
+
+    }
+}
